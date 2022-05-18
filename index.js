5107ff0a982cf7c3a17303f28683a9b7fae7c91c
iinaamasum <iinaamasum@gmail.com>
2022-05-19 05:50:31 +0600

    insert one api done

--- index.js
+++ index.js
@@ -7,6 +7,36 @@ const cors = require('cors');
 app.use(cors());
 app.use(express.json());
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.b7dcs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  await client.connect();
+  const taskCollection = client.db('todoData').collection('todoList');
+
+  try {
+    /**
+     * post a single task
+     * link: http://localhost:5000/task
+     */
+    app.post('/task', async (req, res) => {
+      const data = req.body;
+      const doc = data;
+
+      const result = await taskCollection.insertOne(doc);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
 app.get('/', (req, res) => {
   res.send('TO DO SERVER RUNNING');
 });
